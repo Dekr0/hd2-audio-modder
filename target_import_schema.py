53c6ad4b5054d7a07b6efce1a1320bf4b04840e9
Dekr0 <dekr0.dk@protonmail.com>
2025-01-03 14:26:18 -0700

    - ./target_import_schema.py     - a first draft schema for target importing - ./target_import_schema_test.py     - basic unit test (don't trust too much, catch on production) for       validating the schema of target import

---
+++ target_import_schema.py
@@ -0,0 +1,149 @@
+"""
+All fields in the schema is required so that dict to dataclass is easier to 
+handle.
+"""
+
+from referencing.jsonschema import Schema
+
+
+VERSION = 2
+
+
+revert_schema: Schema = {
+    "$id": "https://github.com/RaidingForPants/hd2-audio-modder/schemas/revert",
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "description": "An object indicate whether if the audio modding tool will "
+                   "revert all audio source to default before / after doing "
+                   "target import.",
+    "type": "object",
+    "properties": {
+        "before": {
+            "description": "An true or false flag to indicate whether if the audio"
+                           " modding tool will revert all audio source to default"
+                           " before doing target import.",
+            "type": "boolean"
+        },
+        "after": {
+            "description": "An true or false flag to indicate whether the audio"
+                           " modding tool will revert all audio source to default"
+                           " audio source after doing target import **and** "
+                           " generating a new patch.",
+            "type": "boolean"
+        }
+    },
+    "required": [
+        "before", "after",
+    ]
+}
+
+target_import_pair_schema: Schema = {
+    "$id": "https://github.com/RaidingForPants/hd2-audio-modder/schemas/target_import_pair",
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "description": "A target import pair",
+    "type": "object",
+    "properties": {
+        "from": {
+            "description": "A relative file path of an audio file under the file "
+                           "path specified by `workspace`.",
+            "type": "string",
+        },
+        "to": {
+            "description": "A list of audio source ID in the active / loaded "
+                           "archive that will be replaced by the audio file "
+                           "specified by field `from`",
+            "type": "array",
+            "items": {
+                "type": "integer"
+            },
+            "minItems": 1
+        }
+    },
+    "required": [
+        "from",
+        "to"
+    ]
+}
+
+target_import_schema: Schema = {
+    "$id": "https://github.com/RaidingForPants/hd2-audio-modder/schemas/target_import",
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "type": "object",
+    "properties": {
+        "workspace": {
+            "description": "A file path that contains all audio files you want "
+                           "to import in this section of target import",
+            "type": "string"
+        },
+        "folders": {
+            "description": "An array of target import section. Make it as an "
+                           "empty array to skip this step.",
+            "type": "array",
+            "items": { "$ref": "#" },
+            "minItems": 0
+        },
+        "pairs": {
+            "description": "An array of target import pairs that will be performed"
+                           "in this section of target import.",
+            "type": "array",
+            "items": target_import_pair_schema,
+            "minItems": 1
+        }
+    },
+    "required": [ "workspace", "folders", "pairs" ]
+}
+
+task_schema: Schema = {
+    "$id": "https://github.com/RaidingForPants/hd2-audio-modder/schemas/task",
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "type": "object",
+    "properties": {
+        "revert_all": revert_schema,
+        "write_patch_to": {
+            "description": "A file path for generate a new patch after finishing"
+                           " target import all the prov$ided audio source and before "
+                           "reverting all audio source back to default. Put an "
+                           " empty path to skip this step",
+            "type": "string",
+        },
+        "target_imports": {
+            "description": "An array of target import the current target import "
+                           "task will perform.",
+            "type": "array",
+            "items": target_import_schema,
+            "minItems": 1
+        }
+    },
+    "required": [
+        "revert_all",
+        "target_imports",
+        "write_patch_to",
+    ]
+}
+
+manifest_schema: Schema = {
+    "$id": "https://github.com/RaidingForPants/hd2-audio-modder/schemas/manifest",
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "type": "object",
+    "properties": {
+        "version": { 
+            "description": "Version of target import manifest",
+            "const": VERSION
+        },
+        "tasks": {
+            "description": "An array of target import tasks that will be run on "
+                           "the current acitve / loaded archive. The order of  "
+                           "of task execution will be in the order they "
+                           "are declared.",
+            "type": "array",
+            "items": task_schema,
+            "minItems": 1
+        }
+    },
+    "required": [ "version", "tasks" ],
+    "$defs": {
+        "revert_all": revert_schema,
+        "task": task_schema,
+        "target_import": target_import_schema,
+        "target_import_pair": target_import_pair_schema
+    }
+}
---
+++ target_import_schema_test.py
@@ -0,0 +1,392 @@
+import unittest
+
+from jsonschema import ValidationError
+
+from jsonschema import validate
+
+from log import logger
+
+from target_import_schema import \
+        revert_schema, manifest_schema, target_import_pair_schema, \
+        target_import_schema, task_schema
+
+
+class TestTargetImportSchema(unittest.TestCase):
+
+    def test_revert_all_ok(self):
+        logger.info("Running revert_all_schema validation test (passing)...")
+        test_cases = [
+            {"before": True, "after": True },
+            {"before": True, "after": False },
+            {"before": False, "after": True },
+            {"before": False, "after": False },
+            {"after": True, "before": True },
+            {"after": True, "before": False },
+            {"after": False, "before": True },
+            {"after": False, "before": False },
+        ]
+
+        for test_case in test_cases:
+            logger.info(f"Validating {test_case}")
+            validate(test_case, revert_schema)
+
+    def test_revert_all_fail(self):
+        logger.info("Running revert_all_schema validation test (failing)...")
+        test_cases = [
+            {"after": True, "after": False},
+            {"afer": True, "beore": True},
+            {"beore": False, "afer": True},
+            {"before": 1, "after": 2},
+        ]
+
+        for test_case in test_cases:
+            logger.info(f"Validating {test_case}")
+            self.assertRaises(
+                ValidationError, 
+                lambda: validate(test_case, revert_schema)
+            )
+
+    def test_target_import_pair_schema_ok(self):
+        logger.info("Running target_import_schema_pair validation test (passing)...")
+        test_cases = [
+            { "from": "audio.wav", "to": [123] },
+            { "from": "audio.wav", "to": [123, 345] },
+            { "to": [900, -123], "from": "vo.wav" },
+        ]
+
+        for test_case in test_cases:
+            logger.info(f"Validating {test_case}")
+            validate(test_case, target_import_pair_schema)
+
+    def test_target_import_pair_schema_fail(self):
+        logger.info("Running target_import_schema_pair validation test (failing)...")
+        test_cases = [
+            # Missing `to`
+            { "from": "audio.wav" },
+
+            # `to` has empty array. Why do you want to have an empty array?
+            { "from": "audio.wav", "to": [] },
+
+            # `to` has a decimal audio source ID
+            { "to": [9.1, 123], "from": "vo.wav" },
+
+            # `from` is a decimal audio source ID
+            { "from": 1, "to": [123, 234] },
+
+            # `to` is using string based integer audio source ID
+            { "from": "audio.wav", "to": ["123", "234"] },
+
+            # `wrong keyword`
+            { "fro": "audio.wav", "too": ["123", "234"] },
+        ]
+
+        for test_case in test_cases:
+            logger.info(f"Validating {test_case}")
+            self.assertRaises(
+                ValidationError, 
+                lambda: validate(test_case, target_import_pair_schema)
+            )
+
+    def test_target_import_schema_ok(self):
+        logger.info("Running target_import_schema validation test (passing)...")
+        test_cases = [
+            {
+                "workspace": "..",
+                "folders": [],
+                "pairs": [
+                    { "from": "audio_01.wav", "to": [123] },
+                    { "from": "audio_02.wav", "to": [123, 345] },
+                ]
+            },
+
+            {
+                "workspace": "..",
+                "folders": [],
+                "pairs": [
+                    { "to": [8080, 8888], "from": "audio_03.wav" },
+                    { "to": [80, 400], "from": "audio_04.wav" },
+                ]
+            },
+
+            {
+                "workspace": "..",
+                "folders": [],
+                "pairs": [
+                    { "to": [8080, 8888], "from": "audio_03.wav" },
+                    { "to": [80, 400], "from": "audio_04.wav" },
+                ]
+            },
+
+            {
+                "workspace": "..",
+                "folders": [
+                    {
+                        "workspace": ".",
+                        "folders": [],
+                        "pairs": [
+                            { "from": "audio_01.wav", "to": [45] },
+                            { "from": "audio_02.wav", "to": [80, 21] },
+                        ]
+                    }
+                ],
+                "pairs": [
+                    { "to": [8080, 8888], "from": "audio_03.wav" },
+                    { "to": [80, 400], "from": "audio_04.wav" },
+                ]
+            }
+        ]
+        for test_case in test_cases:
+            logger.info(f"Validating {test_case}")
+            validate(test_case, target_import_schema)
+
+    def test_target_import_schema_fail(self):
+        logger.info("Running target_import_schema validation test (failing)...")
+        test_cases = [
+            # Empty pairs. Why do you want to make an array of empty pair?
+            {
+                "workspace": "..",
+                "folders": [],
+                "pairs": []
+            },
+
+            # Missing folder. Make it an empty array to skip this step 
+            {
+                "workspace": "..",
+                "pairs": []
+            },
+
+            # Missing workspace
+            {
+                "folders": [],
+                "pairs": [
+                    { "to": [8080, 8888], "from": "audio_03.wav" },
+                    { "to": [80, 400], "from": "audio_04.wav" },
+                ]
+            },
+
+            # Missing workspace in the nested part
+            {
+                "workspace": "..",
+                "folders": [
+                    {
+                        "folders": [],
+                        "pairs": [
+                           { "from": "audio_01.wav", "to": [45] },
+                           { "from": "audio_02.wav", "to": [80, 21] },
+                        ]
+                    }
+                ],
+                "pairs": [
+                    { "to": [8080, 8888], "from": "audio_03.wav" },
+                    { "to": [80, 400], "from": "audio_04.wav" },
+                ]
+            },
+ 
+            # One of the pairs contain decimal audio source ID
+            {
+                "workspace": "..",
+                "folders": [],
+                "pairs": [
+                    { "to": [9.1, 123], "from": "vo.wav" },
+                    { "to": [80, 400], "from": "audio_04.wav" },
+                ]
+            },
+ 
+            # One of the `to` is empty
+            {
+                "workspace": "..",
+                "folders": [
+                    {
+                        "workspace": ".",
+                        "folders": [],
+                        "pairs": [
+                            { "from": "audio.wav", "to": [] },
+                            { "from": "audio_02.wav", "to": [80, 21] },
+                        ]
+                    }
+                ],
+                "pairs": [
+                    { "to": [8080, 8888], "from": "audio_03.wav" },
+                    { "to": [80, 400], "from": "audio_04.wav" },
+                ]
+            },
+
+            {
+                "workspace": "..",
+                "folders": [
+                    {
+                        "workspace": ".",
+                        "folders": [
+                            {
+                                "workspace": "folder_1",
+                                "folders": [],
+                                "pairs": [] # Cannot be empty
+                            }
+                        ],
+                        "pairs": [
+                            { "from": "audio_01.wav", "to": [45] },
+                            { "from": "audio_02.wav", "to": [80, 21] },
+                        ]
+                    }
+                ],
+                "pairs": [
+                    { "to": [8080, 8888], "from": "audio_03.wav" },
+                    { "to": [80, 400], "from": "audio_04.wav" },
+                ]
+            },
+        ]
+        for test_case in test_cases:
+            logger.info(f"Validating {test_case}")
+            self.assertRaises(
+                ValidationError, 
+                lambda: validate(test_case, target_import_schema)
+            )
+
+    def test_target_import_task_schema_ok(self):
+        logger.info("Running target_import_task_schema validation test (passing)...")
+        test_cases = [
+            {
+                "revert_all": { "before": True, "after": True },
+                "write_patch_to": ".",
+                "target_imports": [
+                    {
+                        "workspace": "..",
+                        "folders": [],
+                        "pairs": [
+                            { "from": "audio_01.wav", "to": [123] },
+                            { "from": "audio_02.wav", "to": [123, 345] }
+                        ]
+                    },
+                    {
+                        "workspace": "..",
+                        "folders": [
+                            {
+                                "workspace": ".",
+                                "folders": [],
+                                "pairs": [
+                                    { "from": "audio_01.wav", "to": [45] },
+                                    { "from": "audio_02.wav", "to": [80, 21] },
+                                ]
+                            }
+                        ],
+                        "pairs": [
+                            { "from": "audio_01.wav", "to": [123] },
+                            { "from": "audio_02.wav", "to": [123, 345] }
+                        ]
+                    }
+                ]
+            },
+        ]
+
+        for test_case in test_cases:
+            logger.info(f"Validating {test_case}")
+            validate(test_case, task_schema)
+
+    def test_target_import_task_schema_fail(self):
+        logger.info("Runing target_import_task_schema validation test (failing)...")
+        test_cases = [ 
+            # target_imports has an empty array. Why do you want to have an empty
+            # of target imports?
+            {
+                "revert_all": { "before": True, "after": True },
+                "write_patch_to": ".",
+                "target_imports": []
+            },
+            # Missing revert all
+            {
+                "write_patch_to": ".",
+                "target_imports": [
+                    {
+                        "workspace": "..",
+                        "folders": [],
+                        "pairs": [
+                            { "from": "audio_01.wav", "to": [123] },
+                            { "from": "audio_02.wav", "to": [123, 345] }
+                        ]
+                    }
+                ]
+            },
+            # Missing revert all
+            {
+                "revert_all": { "before": True, "after": True },
+                "target_imports": [
+                    {
+                        "workspace": "..",
+                        "folders": [],
+                        "pairs": [
+                            { "from": "audio_01.wav", "to": [123] },
+                            { "from": "audio_02.wav", "to": [123, 345] }
+                        ]
+                    }
+                ]
+            }
+        ]
+        for test_case in test_cases:
+            logger.info(f"Validating {test_case}")
+            self.assertRaises(
+                ValidationError, 
+                lambda: validate(test_case, target_import_schema)
+            )
+
+
+    def test_schema_ok(self):
+        logger.info("Running schema validation test (passing)...")
+        test_cases = [
+            {
+                "version": 2,
+                "tasks": [
+                    {
+                        "revert_all": { "before": True, "after": True },
+                        "write_patch_to": ".",
+                        "target_imports": [
+                            {
+                                "workspace": "..",
+                                "folders": [],
+                                "pairs": [
+                                    { "from": "audio_01.wav", "to": [123] },
+                                    { "from": "audio_02.wav", "to": [123, 345] }
+                                ]
+                            },
+                            {
+                                "workspace": "..",
+                                "folders": [
+                                    {
+                                        "workspace": ".",
+                                        "folders": [],
+                                        "pairs": [
+                                            { "from": "audio_01.wav", "to": [45] },
+                                            { "from": "audio_02.wav", "to": [80, 21] },
+                                        ]
+                                    }
+                                ],
+                                "pairs": [
+                                    { "from": "audio_01.wav", "to": [123] },
+                                    { "from": "audio_02.wav", "to": [123, 345] }
+                                ]
+                            }
+                        ]
+                    }                
+                ]
+            }
+        ]
+        
+        for test_case in test_cases:
+            logger.info(f"Validating {test_case}")
+            validate(test_case, manifest_schema)
+
+    def test_schema_fail(self):
+        logger.info("Running schema validation test (failing)...")
+        test_cases = [
+            # Wrong version number
+            { "version": 1 },
+        ]
+        for test_case in test_cases:
+            logger.info(f"Validating {test_case}")
+            self.assertRaises(
+                ValidationError, 
+                lambda: validate(test_case, target_import_schema)
+            )
+
+
+if __name__ == "__main__":
+    unittest.main()
