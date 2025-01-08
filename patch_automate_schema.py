aac44a2e18d32d2bfefe1c13a418770b72b7a014
Dekr0 <dekr0.dk@protonmail.com>
2025-01-08 12:35:56 -0700

    - Added field `data` for looking up archive files in an patch automation   task. - Added field `workspace` for looking up target import manifest files in   an patch automation task. - Update field `using` description.

--- patch_automate_schema.py
+++ patch_automate_schema.py
@@ -16,11 +16,23 @@ task: Schema = {
             },
             "minItems": 1
         },
+        "data": {
+            "description": "The file path of a directory for looking up archive "
+                           "files. If this field is ignored, it will use the "
+                           "file path stored in your configuration file, which "
+                           "is the `data` folder for Helldivers 2 in the disk.",
+            "type": "string"
+        },
+        "workspace": {
+            "description": "The file path of a directory for looking up target "
+                           "import automation manifest file.",
+            "type": "string"
+        },
         "using": {
             "description": "An array of target import manifest files (relative "
                            "path or absolute path) you want to apply in the current "
-                           "task. The order of which manifest files are layout "
-                           "will be the order of how manifest files being applied",
+                           "task. Each target import manifest will be applied on a freshly"
+                           " open archive file.",
             "type": "array",
             "items": {
                 "type": "string"
@@ -28,10 +40,10 @@ task: Schema = {
             "minItems": 1
         }
     },
-    "required": [ "archive_files", "using" ]
+    "required": [ "archive_files", "workspace", "using" ]
 }
 
-manifest: Schema = {
+manifest_schema: Schema = {
     "$id": "https://github.com/RaidingForPants/hd2-audio-modder/schemas/patch/manifest",
     "$schema": "https://json-schema.org/draft/2020-12/schema",
     "type": "object",
