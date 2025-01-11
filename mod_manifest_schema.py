9378082d3b2606b1edf0dcfe60f694740b903ee4
Dekr0 <dekr0.dk@protonmail.com>
2025-01-11 00:54:57 -0700

    constant and template for mod manifest

---
+++ mod_manifest_schema.py
@@ -0,0 +1,13 @@
+manifest_template = {
+    "version": 1,
+    "Guid": "",
+    "Name": "",
+    "Description": "",
+    "Options": [
+        {
+            "Name": "",
+            "Description": "",
+            "Include": [ "main_variant" ] 
+        }
+    ]
+}
