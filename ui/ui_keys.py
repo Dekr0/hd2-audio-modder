6f86963c711a67152ad48cc5708ee4fc4711ed24
Dekr0 <dekr0.dk@protonmail.com>
2025-01-13 13:01:20 -0700

    IDing for different ImGUI components

---
+++ ui/ui_keys.py
@@ -0,0 +1,8 @@
+import enum
+
+
+class WidgetKey(enum.StrEnum):
+    LOAD_ARCHIVE_FROM_DATA = "LoadArchiveFromData"
+    LOAD_ARCHIVE_FROM_FILE_EXPLORER = ""
+
+    BANK_HIERARCHY_TABLE = "bank_hierarchy_table"
