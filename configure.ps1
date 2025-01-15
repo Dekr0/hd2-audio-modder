f638baa6d67c1aae276e27c8952116aaf07539f7
Dekr0 <dekr0.dk@protonmail.com>
2025-01-15 03:33:22 -0700

    update configuration

--- configure.ps1
+++ configure.ps1
@@ -2,6 +2,12 @@ python -m venv .venv
 
 . .\.venv\Scripts\Activate.ps1
 
+wget -Uri "https://github.com/vgmstream/vgmstream-releases/releases/download/nightly/vgmstream-win64.zip" -OutFile .\vgmstream-win64.zip
+
+Expand-Archive -Path ".\vgmstream-win64.zip" -DestinationPath ".\vgmstream-win64"
+
+rm .\vgmstream-win64.zip
+
 pip install -r requirements.txt
 
 deactivate
