1270fe0710863cafc12996ad74c94e60158699de
Dekr0 <dekr0.dk@protonmail.com>
2025-01-27 15:07:45 -0700

    Renaming

--- ui/bank_viewer/tree_selection_impl.py
+++ ui/bank_viewer/tree_selection_impl.py
@@ -1,7 +1,8 @@
 from imgui_bundle import imgui
 
-from ui.view_data import BankViewerTableType, TREE_ROOT_VIEW_ID
-from ui.view_data import BankViewerState, HircView
+from ui.bank_viewer.state import BankViewerTableType, TREE_ROOT_VID
+from ui.bank_viewer.state import BankViewerState, HircView
+
 
 """
 Below implementation are directly one-to-one from ImGui Manual. ImGui does not
@@ -9,24 +10,21 @@ have an implementation that support multi selection for tree view. This
 implementation emulate the basic selection in Tkinter.
 """
 def tree_node_get_open(node: HircView):
-    return imgui.get_state_storage().get_bool(node.view_id)
+    return imgui.get_state_storage().get_bool(node.vid)
 
 
 def tree_node_set_open(node: HircView, open: bool):
-    return imgui.get_state_storage().set_bool(node.view_id, open)
+    return imgui.get_state_storage().set_bool(node.vid, open)
 
 
 def tree_close_and_unselected_child_nodes(
-    node: HircView,
-    selection: imgui.SelectionBasicStorage,
-    depth: int
-    ):
+    node: HircView, selection: imgui.SelectionBasicStorage, depth: int):
     """
     When closing a node:
     1. close and unselect all children
     2. select parent if any child was selected
     """
-    unselected_count = 1 if selection.contains(node.view_id) else 0
+    unselected_count = 1 if selection.contains(node.vid) else 0
     if depth == 0 or tree_node_get_open(node):
         for c in node.children:
             unselected_count += tree_close_and_unselected_child_nodes(
@@ -34,7 +32,7 @@ def tree_close_and_unselected_child_nodes(
             tree_node_set_open(node, False)
 
     # Select root node if any of its child was selected, otherwise unselect
-    selection.set_item_selected(node.view_id, depth == 0 and unselected_count > 0)
+    selection.set_item_selected(node.vid, depth == 0 and unselected_count > 0)
     return unselected_count
 
 
@@ -63,26 +61,26 @@ def apply_selection_reqs(
                     if source_view:
                         if linear_mapping[i].hirc_obj_type in source_view_selectable:
                             imgui_selection_store.set_item_selected(
-                                linear_mapping[i].view_id, req.selected)
+                                linear_mapping[i].vid, req.selected)
                     else:
                         imgui_selection_store.set_item_selected(
-                                linear_mapping[i].view_id, req.selected)
+                                linear_mapping[i].vid, req.selected)
             else:
                 imgui_selection_store.clear()
         elif req.type == imgui.SelectionRequestType.set_range:
             head = req.range_first_item
             tail = req.range_last_item
             for i in range(head, tail + 1):
-                if linear_mapping[i].view_id == TREE_ROOT_VIEW_ID:
+                if linear_mapping[i].vid == TREE_ROOT_VID:
                     raise AssertionError(f"Invisible root node is inside the "
                                          "selection scheme")
                 if source_view:
                     if linear_mapping[i].hirc_obj_type in source_view_selectable:
                         imgui_selection_store.set_item_selected(
-                            linear_mapping[i].view_id, req.selected)
+                            linear_mapping[i].vid, req.selected)
                 else:
                     imgui_selection_store.set_item_selected(
-                        linear_mapping[i].view_id, req.selected)
+                        linear_mapping[i].vid, req.selected)
 
 """
 End of Tree view selection
