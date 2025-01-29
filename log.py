0347d060e368f2120962d28c65a903eba5a10bdb
Dekr0 <dekr0.dk@protonmail.com>
2025-01-28 21:45:23 -0700

    log setting changed

--- log.py
+++ log.py
@@ -22,11 +22,15 @@ def get_logger():
 
         logger = logging.getLogger()
 
-        logger.setLevel(logging.DEBUG)
+        logger.setLevel(logging.INFO)
         
         file_handler = logging.FileHandler("log.txt")
         file_handler.setFormatter(std_formatter)
         logger.addHandler(file_handler)
+
+        # stream_handler = logging.StreamHandler()
+        # stream_handler.setFormatter(std_formatter)
+        # logger.addHandler(stream_handler)
 
         return logger
 
