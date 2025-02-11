97c262564fd21e9346a4fbda9b204ec4adf9b63b
Dekr0 <dekr0.dk@protonmail.com>
2025-02-11 05:10:53 -0700

    basic test for json automation

--- tests/target_import_automate_test.py
+++ tests/target_import_automate_test.py
@@ -3,17 +3,27 @@ import posixpath as xpath
 import unittest
 
 from target_import_csv import target_import_automation_csv
+from target_import_json import target_import_automation_json
 from core import Mod
 from env import get_data_path
 
 
 class TargetImportAutomateTest(unittest.IsolatedAsyncioTestCase):
 
+    """
     async def test_target_import_csv(self):
         csvs: list[str] = [entry.path for entry in os.scandir("tests/mockup/csvs")]
         for csv in csvs:
             mod = Mod("")
             archive = csv.split("_")[-1].strip(".csv")
-            print(csv)
             mod.load_archive_file(xpath.join(get_data_path(), archive))
             await target_import_automation_csv(mod, csv)
+    """
+
+    async def test_target_import_json(self):
+        jsons: list[str] = [entry.path for entry in os.scandir("tests/mockup/jsons")]
+        for j in jsons:
+            mod = Mod("")
+            archive = j.split("_")[-1].strip(".json")
+            mod.load_archive_file(xpath.join(get_data_path(), archive))
+            await target_import_automation_json(mod, j)
