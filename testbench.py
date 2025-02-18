06a6b63951384bec397fda4728b72db3075875a3
Dekr0 <dekr0.dk@protonmail.com>
2025-02-18 03:26:22 -0700

    resolve conflict in testbench

--- testbench.py
+++ testbench.py
@@ -1,11 +1,19 @@
+import os
+import shutil
 import unittest
+
+from env import TMP
 
 # from tests.actor_mixer_test import TestActorMixerParser
 # from tests.sound_parser_test import TestSoundParser
 # from tests.cntr_parser_test import TestCntrParser
 # from tests.parser_integration_test import TestParserIntegration
 # from tests.mediautil_test import TestMediaUtil
+# from tests.mediautil_test import TestMediaUtil
+# from tests.mod_test import TestMod
 
 
 if __name__ == "__main__":
+    os.mkdir(TMP)
     unittest.main()
+    shutil.rmtree(TMP)
