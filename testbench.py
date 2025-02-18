8b4958e2a45cc6de3d8be9bba0dcefe96096c373
Dekr0 <dekr0.dk@protonmail.com>
2025-02-18 03:28:11 -0700

    resolve conflict in testbench

--- testbench.py
+++ testbench.py
@@ -15,6 +15,9 @@ from tests.parser_integration_test import TestParserIntegration
 # from tests.mediautil_test import TestMediaUtil
 # from tests.mod_test import TestMod
 # from tests.sound_handler_test import TestSoundHandler
+# from tests.patch_automate_schema_test import TestPatchAutomateSchema
+# from tests.target_import_schema_test import TestTargetImportSchema
+# from tests.target_import_automate_test import TargetImportAutomateTest
 
 
 if __name__ == "__main__":
