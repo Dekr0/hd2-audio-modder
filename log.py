e66cfd142c50071cb6a9b0eaad0559e2b1cb4821
Dekr0 <dekr0.dk@protonmail.com>
2025-02-19 23:15:34 -0700

    update log level

--- log.py
+++ log.py
@@ -27,7 +27,7 @@ def get_logger():
 
         logger = logging.getLogger()
 
-        logger.setLevel(logging.CRITICAL)
+        logger.setLevel(logging.INFO)
         
         logger.addHandler(default_file_handler)
         logger.addHandler(default_stream_handler)
