70a8ffd5b3e2d8d90bb0a85fba1e1194df2ead79
Dekr0 <dekr0.dk@protonmail.com>
2025-03-13 02:38:21 -0600

    resolve rebase conflict

--- testbench.py
+++ testbench.py
@@ -1,6 +1,10 @@
+import os
+import shutil
 import unittest
 
-# from tests.actor_mixer_parser_test import TestActorMixerParser
+from env import TMP
+
+# from tests.actor_mixer_test import TestActorMixerParser
 # from tests.sound_parser_test import TestSoundParser
 # from tests.cntr_parser_test import TestCntrParser
 from tests.parser_integration_test import TestParserIntegration
@@ -9,7 +13,11 @@ from tests.parser_integration_test import TestParserIntegration
 # from tests.event_parser_test import TestEventParser
 # from tests.action_parser_test import TestActionParser
 # from tests.mediautil_test import TestMediaUtil
+# from tests.mediautil_test import TestMediaUtil
+# from tests.mod_test import TestMod
 
 
 if __name__ == "__main__":
+    os.mkdir(TMP)
     unittest.main()
+    shutil.rmtree(TMP)
