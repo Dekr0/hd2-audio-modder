b5001c3d8f5b9ebef07dbce9a80841fdbb79a3bd
Dekr0 <dekr0.dk@protonmail.com>
2025-03-22 02:18:51 -0600

    change log level

--- log.py
+++ log.py
@@ -27,7 +27,7 @@ def get_logger():
 
         logger = logging.getLogger()
 
-        logger.setLevel(logging.INFO)
+        logger.setLevel(logging.WARNING)
         
         logger.addHandler(default_file_handler)
         logger.addHandler(default_stream_handler)
